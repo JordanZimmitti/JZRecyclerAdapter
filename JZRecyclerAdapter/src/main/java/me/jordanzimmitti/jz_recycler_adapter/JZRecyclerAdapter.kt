f61e596cf8c2c43665761544e85589590015a7cc
JordanZimmitti <jordanzimmitti@gmail.com>
2019-03-27 22:42:07 -0400

    Cleaned up code for official release

--- JZRecyclerAdapter/src/main/java/me/jordanzimmitti/jz_recycler_adapter/JZRecyclerAdapter.kt
+++ JZRecyclerAdapter/src/main/java/me/jordanzimmitti/jz_recycler_adapter/JZRecyclerAdapter.kt
@@ -1,4 +1,4 @@
-package me.jordanzimmitti.recyclerview
+package me.jordanzimmitti.jz_recycler_adapter
 
 import android.graphics.Rect
 import android.view.LayoutInflater
@@ -12,13 +12,13 @@ private typealias ItemClicked = (position: Int, view: View)                     
 private typealias MultiSelect = (itemSelected: Int, allItemSelected: Int, isSelected: Boolean) -> Unit
 private typealias Scroll      = (dy: Int)                                                      -> Unit
 
-/** Kotlin Class RecyclerAdapter
+/** Kotlin Class JZRecyclerAdapter
  *
- * Class That Creates An Adapter To Show All The Items In A List For A Given Type
+ * Class That Creates An Adapter To Show All The Items In A RecyclerView List For A Given Type
  *
  * @author Jordan Zimmitti
  *
- * @version 6.0.0
+ * @version 2.0.0
  */
 class JZRecyclerAdapter<TYPE>(private val layoutId: Int,
                               private val list    : List<TYPE>,
@@ -37,29 +37,14 @@ class JZRecyclerAdapter<TYPE>(private val layoutId: Int,
         private var scroll     : Scroll?      = null
 
         /**.
-         * Function That Handles When The RecyclerView Is Scrolling
-         *
-         * @param scroll       The code for when the recycler view is scrolling
-         * @param recyclerView Any recycler view
-         */
-        fun whenScrolling(recyclerView: RecyclerView, scroll: Scroll) {
-
-            // Sets The Value For this.scroll//
-            this.scroll = scroll
-
-            // Starts Function//
-            onScrolled(recyclerView)
-        }
-
-        /**.
-         * Function That Handles When The RecyclerAdapter Is Multi Selected
+         * Function That Gets The Code For When The RecyclerAdapter Is Multi Selected
          *
          * @param recyclerAdapter Any recycler adapter
          * @param multiSelect     The code for when the recycler adapter is multi selected
          */
         fun multiSelected(recyclerAdapter: JZRecyclerAdapter<*>, multiSelect: MultiSelect) {
 
-            // Sets The Value For this.multiSelect//
+            // Sets The Value For The Class Variable//
             this.multiSelect = multiSelect
 
             // Starts Function//
@@ -67,43 +52,65 @@ class JZRecyclerAdapter<TYPE>(private val layoutId: Int,
         }
 
         /**.
-         * Function That Handles The OnScrollListener
+         * Function That Gets The Code For When The RecyclerView Is Scrolling
+         *
+         * @param scroll       The code for when the recycler view is scrolling
+         * @param recyclerView Any recycler view
+         */
+        fun whenScrolling(recyclerView: RecyclerView, scroll: Scroll) {
+
+            // Sets The Value For The Class Variable//
+            this.scroll = scroll
+
+            // Starts Function//
+            onScrolled(recyclerView)
+        }
+
+        /**.
+         * Function That Handles When The RecyclerView Is Scrolling
          *
          * @param recyclerView Any recycler view
          */
         private fun onScrolled(recyclerView: RecyclerView) {
 
+            // When The RecyclerView Is Scrolling//
             recyclerView.addOnScrollListener(object: RecyclerView.OnScrollListener() {
                 override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
                     super.onScrolled(recyclerView, dx, dy)
 
+                    // The Code When Scrolling//
                     scroll?.invoke(dy)
                 }
             })
         }
 
         /**.
-         * Function That Handles The OnMultiChoiceSelectionListener
+         * Function That Handles When An Item Is MultiSelected
          *
          * @param recyclerAdapter Any recycler adapter
          */
         private fun onMultiSelected(recyclerAdapter: JZRecyclerAdapter<*>) {
 
+            // When An Item Is Multi Selected//
             recyclerAdapter.setMultiChoiceSelectionListener (object: MultiChoiceAdapter.Listener     {
 
+                // When An Item Is Selected//
                 override fun OnItemSelected(selectedPosition: Int, itemSelectedCount: Int, allItemCount: Int) {
 
+                    // The Code For When An Item Is Selected//
                     multiSelect?.invoke(selectedPosition, itemSelectedCount, true)
                 }
 
+                // When An Item Is De-Selected//
                 override fun OnItemDeselected(deselectedPosition: Int, itemSelectedCount: Int, allItemCount: Int) {
 
+                    // The Code For When An Item Is De-Selected//
                     multiSelect?.invoke(deselectedPosition, itemSelectedCount, false)
                 }
 
+                // Not Implemented Yet...//
                 override fun OnSelectAll(itemSelectedCount: Int, allItemCount: Int) {
                 }
-
                 override fun OnDeselectAll(itemSelectedCount: Int, allItemCount: Int) {
                 }
             })
@@ -147,7 +154,7 @@ class JZRecyclerAdapter<TYPE>(private val layoutId: Int,
         // Returns The On Click Action//
         return View.OnClickListener { view ->
 
-            // Differ Action To Another Class Instance//
+            // Code For When The Item Is Clicked//
             itemClicked?.invoke(position, view)
         }
     }
@@ -160,21 +167,21 @@ class JZRecyclerAdapter<TYPE>(private val layoutId: Int,
      */
     override fun onBindViewHolder(holder: JZRecyclerAdapter<TYPE>.ViewHolder, position: Int) {
 
-        // Differ To Parent Class//
+        // Differs To Parent Class//
         super.onBindViewHolder(holder, position)
 
-        // Set The Item To Its Corresponding View//
+        // Sets The Item To Its Corresponding View//
         holder.itemView.views(list[position])
     }
 
     /**.
-     * Function That Sets The Value For When An Item is Clicked
+     * Function That Gets The Code For When An Item is Clicked
      *
-     * @param itemClick Action from the caller class
+     * @param itemClick The code for when the item is clicked
      */
     fun setItemClick(itemClick: ItemClicked) {
 
-        // Sets The Value For this.itemClick//
+        // Sets The Value For The Class Variable//
         this.itemClicked = itemClick
     }
 
@@ -192,10 +199,10 @@ class JZRecyclerAdapter<TYPE>(private val layoutId: Int,
          */
         override fun getItemOffsets(spacing: Rect, view: View, parent: RecyclerView, c: RecyclerView.State) {
 
-            // Set The Spacing//
+            // Sets The Spacing//
             spacing.bottom = height
 
-            // Set The Spacing For The Last Item//
+            // Sets The Spacing For The Last Item//
             if (parent.getChildAdapterPosition(view) == parent.adapter!!.itemCount - 1) spacing.bottom = 280
         }
     }
@@ -204,7 +211,6 @@ class JZRecyclerAdapter<TYPE>(private val layoutId: Int,
      * Class That Sets Up The Views To Show The Lists Items
      *
      * @param customView Custom View That Shows The Lists Items
-     *
      */
     inner class ViewHolder(customView: View): RecyclerView.ViewHolder(customView)
 }
